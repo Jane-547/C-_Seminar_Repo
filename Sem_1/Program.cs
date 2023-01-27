385b6aaf7a5a8d17d22a1e9cd4d6abe78e509dea
Jane-547 <Jane-547@bk.com>
2023-01-27 19:56:35 +0300

    Code optimized

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -5,6 +5,7 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+System.Console.WriteLine("Задача 2");
 System.Console.Write("Введите первое число: ");
 string strNumber1_1 = Console.ReadLine();
 int Number1_1 = Convert.ToInt32(strNumber1_1);
@@ -13,15 +14,13 @@ System.Console.Write("Введите второе число: ");
 string strNumber1_2 = Console.ReadLine();
 int Number1_2 = Convert.ToInt32(strNumber1_2);
 
-System.Console.Write("max = ");
-
 if (Number1_1 > Number1_2)
 {
-  System.Console.WriteLine(Number1_1);
+  System.Console.WriteLine("max = " + Number1_1);
 }
 else
 {
-    System.Console.WriteLine(Number1_2);
+    System.Console.WriteLine("max = " + Number1_2);
 }
 
 // Конец задачи 2.
@@ -33,6 +32,7 @@ else
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+System.Console.WriteLine("Задача 4");
 System.Console.Write("Введите первое число: ");
 string strNumber2_1 = Console.ReadLine();
 int Number2_1 = Convert.ToInt32(strNumber2_1);
@@ -56,8 +56,7 @@ if (Number2_3 > max)
   max = Number2_3;
 }
 
-Console.Write("max = ");
-Console.WriteLine(max);
+Console.WriteLine("max = " + max);
 
 // Конец задачи 4.
 
@@ -68,6 +67,7 @@ Console.WriteLine(max);
 // -3 -> нет
 // 7 -> нет
 
+System.Console.WriteLine("Задача 6");
 System.Console.Write("Введите число: ");
 string strNumber3_1 = Console.ReadLine();
 int Number3_1 = Convert.ToInt32(strNumber3_1);
@@ -89,6 +89,8 @@ else
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+
+System.Console.WriteLine("Задача 8");
 System.Console.Write("Введите число: ");
 string strNumber4_1 = Console.ReadLine();
 int Number4_1 = Convert.ToInt32(strNumber4_1);
@@ -110,7 +112,7 @@ else
 
 while (count <= Number4_1)
 {
-        System.Console.WriteLine(count);
+        System.Console.Write(count + ", ");
         count = count + 2;
 }
 }
