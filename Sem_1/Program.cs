d3223dad7711d2439263390d020d5abea8e4dec4
Jane-547 <Jane-547@bk.com>
2023-01-28 17:46:17 +0300

    Second version of task 8 added

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -16,7 +16,7 @@ int Number1_2 = Convert.ToInt32(strNumber1_2);
 
 if (Number1_1 > Number1_2)
 {
-  System.Console.WriteLine("max = " + Number1_1);
+    System.Console.WriteLine("max = " + Number1_1);
 }
 else
 {
@@ -49,11 +49,11 @@ int max = Number2_1;
 
 if (Number2_2 > max)
 {
-  max = Number2_2;
+    max = Number2_2;
 }
 if (Number2_3 > max)
 {
-  max = Number2_3;
+    max = Number2_3;
 }
 
 Console.WriteLine("max = " + max);
@@ -74,11 +74,11 @@ int Number3_1 = Convert.ToInt32(strNumber3_1);
 
 if (Number3_1 % 2 == 0)
 {
-  System.Console.WriteLine("Да");
+    System.Console.WriteLine("Да");
 }
 else
 {
-  System.Console.WriteLine("Нет");
+    System.Console.WriteLine("Нет");
 }
 
 // Конец задачи 6.
@@ -89,7 +89,6 @@ else
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-
 System.Console.WriteLine("Задача 8");
 System.Console.Write("Введите число: ");
 string strNumber4_1 = Console.ReadLine();
@@ -97,19 +96,47 @@ int Number4_1 = Convert.ToInt32(strNumber4_1);
 
 if (Number4_1 <= 1)
 {
-System.Console.WriteLine("Решений нет");
-return;
+    System.Console.WriteLine("Решений нет");
+    return;
 }
 
 else
 {
-  int count = 2;
+    int count = 2;
 
-while (count <= Number4_1)
-{
+    while (count <= Number4_1)
+    {
         System.Console.Write(count + ", ");
         count = count + 2;
-}
+    }
 }
 
+System.Console.WriteLine();
+
 // Конец задачи 8.
+
+// Второе решение задачи 8. Включая отрицательные N.
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе
+// показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Задача 8. Решение 2");
+System.Console.Write("Введите число: ");
+string strNumber4_2 = Console.ReadLine();
+int Number4_2 = Convert.ToInt32(strNumber4_2);
+
+
+int count2 = 2;
+int res = 0;
+
+if (Number4_2 == 1) System.Console.WriteLine("Решений нет");
+if (Number4_2 <= 0) count2 *= -1;
+if (Number4_2 > 0) res = 2;
+    
+while (Math.Abs(res) <= Math.Abs(Number4_2))
+{
+        System.Console.Write(res + ", ");
+        res += count2;
+}
