4e3fa573c3caa0e62304f1afbcdca1207c6aba6c
Jane-547 <Jane-547@bk.com>
2023-02-03 18:20:28 +0300

    Homework 3 modified

--- Sem_3/Program.cs
+++ Sem_3/Program.cs
@@ -7,6 +7,7 @@
 23432 -> да
 */
 
+System.Console.WriteLine("-------Задача № 19-------");
 System.Console.Write("Введите число: ");
 int Number = int.Parse(Console.ReadLine());
 int invert = 0;
@@ -14,9 +15,9 @@ int temp = Number;
 
 while (temp > 0)
 {
-  int intermediate = temp % 10;
-  temp = temp / 10;
-  invert = invert * 10 + intermediate;
+    int intermediate = temp % 10;
+    temp = temp / 10;
+    invert = invert * 10 + intermediate;
 }
 
 if (Number == invert) System.Console.WriteLine("Число является палиндромом");
@@ -51,7 +52,7 @@ double coordyB = double.Parse(Console.ReadLine());
 System.Console.WriteLine("Координата z");
 double coordzB = double.Parse(Console.ReadLine());
 
-System.Console.WriteLine($"Расстояние между точками = {Math.Sqrt(Math.Pow((coordxB-coordxA), 2)+Math.Pow((coordyB-coordyA), 2)+Math.Pow((coordzB-coordzA), 2))}");
+System.Console.WriteLine($"Расстояние между точками = {Math.Sqrt(Math.Pow((coordxB - coordxA), 2) + Math.Pow((coordyB - coordyA), 2) + Math.Pow((coordzB - coordzA), 2))}");
 
 /*
 Задача 23
@@ -66,10 +67,10 @@ System.Console.WriteLine("-------Задача № 23-------");
 System.Console.Write("Введите число: ");
 int NumberN = int.Parse(Console.ReadLine());
 
-double Cube=1;
+double Cube = 1;
 
-while (Cube<=NumberN)
+while (Cube <= NumberN)
 {
-    System.Console.Write(Math.Pow(Cube,3)+" ");
+    System.Console.Write(Math.Pow(Cube, 3) + " ");
     Cube++;
 }
