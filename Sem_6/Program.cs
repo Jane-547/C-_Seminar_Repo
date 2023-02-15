f358dcdd19e12988c747fd48f0249ed79d8c4207
Jane-547 <Jane-547@bk.ru>
2023-02-15 15:48:53 +0300

    Homework 6 added. Task 1

---
+++ Sem_6/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+System.Console.WriteLine("Введите 5 целых чисел через пробел:");
+  string values = Console.ReadLine();
+    
+  string[] stringValues = values.Split(' ');
+
+
+
+int ArrayFromString (string [] array)
+{
+  int [] outValues = new int [5];
+
+  for (int i = 0; i < array.Length; i++)
+  {
+      outValues [i] = int.Parse(array [i]);
+  }
+  return outValues;
+}
+
+int CountPositiveNumbers(int[] array)  // Метод для подсчета количества положительных элементов
+{
+    int count = 0;
+    foreach (var i in array)
+    {
+        i > 0 ? count++ : 0;
+    }
+    return count;
+}
+
+ArrayFromString(stringValues);
+CountPositiveNumbers(ArrayFromString);
+
+System.Console.WriteLine($"Пользователь ввел {CountPositiveNumbers} чисел больше 0");
